720fd9225ab4ba9c4ec753fe0f43c5eb289c1f01
Baxarev <baxarev.ilya@mail.ru>
2023-03-03 17:15:26 +0300

    Task.js solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,32 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let discriminant = b**2-4*a*c;
+
+  if(discriminant < 0) {
+    return arr
+  }
+
+  if(discriminant === 0) {
+    arr.push(-b / (2*a))
+    return arr
+  }
+
+  if(discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a))
+    return arr
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if(isNaN(percent) || isNaN(contribution) || isNaN(amount) ) {
+    return false
+  }
+
+  let p = percent / 100 / 12;
+  let s = amount - contribution;
+  let payMonths = s * (p + (p / (((1 + p)**countMonths) - 1)));
+
+  return +(payMonths * countMonths).toFixed(2)
+
 }
