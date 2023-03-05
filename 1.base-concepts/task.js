b128f3a619ab357c71409818e8854216dc808cce
Baxarev <baxarev.ilya@mail.ru>
2023-03-05 18:18:17 +0300

    editing task.js1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,19 +3,15 @@ function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = b**2-4*a*c;
 
-  if(discriminant < 0) {
-    return arr
-  }
-
   if(discriminant === 0) {
     arr.push(-b / (2*a))
-    return arr
   }
 
   if(discriminant > 0) {
-    arr.push((-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a))
-    return arr
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a)) 
   }
+
+  return arr
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
