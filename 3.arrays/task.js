b14edd63aa78aaee5a59bf6ab3753e5666900056
Baxarev <baxarev.ilya@mail.ru>
2023-03-15 03:36:40 +0300

    HM 3 arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((el, i) => el === arr2[i] && arr1.length === arr2.length)
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  if ((gender === "мужской" || gender === "женский") && users.length > 0) {
+    return users
+    .filter(el => el.gender === gender)
+    .map(el => el.age)
+    .reduce((acc, el, i, arr) => acc + el / arr.length, 0)
+  }
   
+  return 0
 }
