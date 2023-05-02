35d1c9105e6f1a8447e65e5054effbcc2b64982a
Baxarev <baxarev.ilya@mail.ru>
2023-05-03 01:35:43 +0300

    decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,60 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  const maxCacheValuesCount = 5;
+  return (...args) => {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кеша: ", objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({
+      hash,
+      value: result
+    });
+    if (cache.length > maxCacheValuesCount) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+function sum(a) {
+  console.log(a + "ol");
 }
+
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if(!timeoutId) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    if(timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      func(...args);
+    }, delay)
+  }
+  
+  return wrapper
+}
+
+const myFn = debounceDecoratorNew(sum, 1000);
+
+
